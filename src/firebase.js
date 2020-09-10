a1be4d878b9372393ed8cdd42b6d635cfdc738d4
Marwan Rizik <marwan.rizik@gmail.com>
2020-09-10 16:46:39 +0300

    first commit Header >> ready checkout >> ready product >> ready

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD9Amu-F52Mb7ZClOq8H7ShnpIczGouobA",
+    authDomain: "clone-f38c5.firebaseapp.com",
+    databaseURL: "https://clone-f38c5.firebaseio.com",
+    projectId: "clone-f38c5",
+    storageBucket: "clone-f38c5.appspot.com",
+    messagingSenderId: "226039005231",
+    appId: "1:226039005231:web:cb77c4d2685f5383f686da",
+    measurementId: "G-C2P4T5DXJ1"
+  };
